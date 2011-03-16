6da1f5a9a35244da88a9adc8d2343718833b92ef
Dorian Raymer <deldotdr@gmail.com>
2011-03-16 08:52:32 -0700

    Bump version

--- carrot/__init__.py
+++ carrot/__init__.py
@@ -1,5 +1,5 @@
 """AMQP Messaging Framework for Python"""
-VERSION = (0, 10, 13)
+VERSION = (0, 10, 14)
 __version__ = ".".join(map(str, VERSION)) + "+txamqp"
 __author__ = "Ask Solem"
 __contact__ = "askh@opera.com"
